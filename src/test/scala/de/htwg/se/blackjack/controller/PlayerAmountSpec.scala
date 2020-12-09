7bd1d25310eb4355b9997edc2ce2843b1394c2aa
Daniel Wollmann <danielw77@hotmail.de>
2020-12-09 20:50:56 +0100

    Added test specs

--- src/test/scala/de/htwg/se/blackjack/controller/PlayerAmountSpec.scala
+++ src/test/scala/de/htwg/se/blackjack/controller/PlayerAmountSpec.scala
@@ -1,5 +1,33 @@
 package de.htwg.se.blackjack.controller
 
-class PlayerAmountSpec {
+import de.htwg.se.blackjack.model.{Card, Deck, GameConfig, Hand, Player, Rank, Suit}
+import de.htwg.se.blackjack.util.UndoManager
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
+class PlayerAmountSpec extends AnyWordSpec with Matchers {
+    val undoManager = new UndoManager
+    var deck = new Deck()
+    deck = Deck(deck.initDeck())
+    val controller = new Controller(deck)
+
+    val templateGameConfig = GameConfig(Vector[Player](), Player("Dealer", Hand(Vector[Card]())), deck.resetDeck(), 0, Vector[Player]())
+    "doStep" in {
+        controller.gameConfig = templateGameConfig
+        undoManager.doStep(new PlayerAmountCommand(controller, 3))
+        controller.gameConfig.players.size should be(3)
+    }
+    "undoStep" in {
+        controller.gameConfig = templateGameConfig
+        undoManager.doStep(new PlayerAmountCommand(controller, 3))
+        undoManager.undoStep
+        controller.gameConfig.players.size should be(0)
+    }
+    "redoStep" in {
+        controller.gameConfig = templateGameConfig
+        undoManager.doStep(new PlayerAmountCommand(controller, 3))
+        undoManager.undoStep
+        undoManager.redoStep
+        controller.gameConfig.players.size should be(3)
+    }
 }
